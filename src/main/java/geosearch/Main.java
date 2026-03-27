3399299ddeaecf0b0eaa2f52d56384b1e253bd48
entozhevlad <vladislavzarubin113@gmail.com>
2026-03-27 21:20:07 +0300

    main and bench

--- src/main/java/geosearch/Main.java
+++ src/main/java/geosearch/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Quadtree quadtree = new Quadtree();
 
-        GeoPoint point1 = new GeoPoint(1, 55.7522, 37.6156);
-        GeoPoint point2 = new GeoPoint(2, 55.7539, 37.6208);
-        GeoPoint point3 = new GeoPoint(3, 55.7649, 37.5931);
-        GeoPoint point4 = new GeoPoint(4, 55.7100, 37.5400);
-        GeoPoint point5 = new GeoPoint(5, 59.9386, 30.3141);
+        GeoPoint point1 = new GeoPoint(1, 88, 99);
+        GeoPoint point2 = new GeoPoint(2, 88, 100);
+        GeoPoint point3 = new GeoPoint(3, 77, 66);
+        GeoPoint point4 = new GeoPoint(4, 66, 77);
+        GeoPoint point5 = new GeoPoint(5, 55, 44);
 
         quadtree.insert(point1);
         quadtree.insert(point2);
@@ -22,12 +22,12 @@ public class Main {
         System.out.println();
 
         System.out.println("Точки в радиусе");
-        List<GeoPoint> radiusPoints = quadtree.searchInRadius(55.7522, 37.6156, 0.02);
+        List<GeoPoint> radiusPoints = quadtree.searchInRadius(88, 99, 2);
         printPoints(radiusPoints);
         System.out.println();
 
         System.out.println("Точки в области");
-        List<GeoPoint> boxPoints = quadtree.searchInBox(55.74, 37.59, 55.77, 37.63);
+        List<GeoPoint> boxPoints = quadtree.searchInBox(70, 60, 90, 101);
         printPoints(boxPoints);
     }
 
